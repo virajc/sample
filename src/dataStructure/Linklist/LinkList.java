967dd9682971d07681ddfd632a5e5912459f1fcb
virajc <viraj.chautmal@gmail.com>
2016-05-11 14:32:17 +0530

    Linklist Class Created

---
+++ src/dataStructure/Linklist/LinkList.java
@@ -0,0 +1,14 @@
+package dataStructure.Linklist;
+
+public class LinkList {
+	Node head;
+	class Node{
+		int data;
+		Node next;
+		Node(int data)
+		{
+			this.data = data;
+			this.next = null;
+		}
+	}
+}
