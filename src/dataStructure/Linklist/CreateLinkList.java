a2737c74f6f065377fd8bfbc3774341bfe353d5c
virajc <viraj.chautmal@gmail.com>
2016-05-12 17:45:41 +0530

    LinkList Updated

--- src/dataStructure/Linklist/CreateLinkList.java
+++ src/dataStructure/Linklist/CreateLinkList.java
@@ -17,14 +17,145 @@ public class CreateLinkList {
 		
 		printLinkList(llist);
 		llist = add(llist, 8);
-		printLinkList(llist);
+		//printLinkList(llist);
 		llist = addLast(llist, 30);
-		printLinkList(llist);
+		llist = addLast(llist, 50);
+		llist = addLast(llist, 38);
+		//printLinkList(llist);
 		llist = addafter(llist, 35, secondNode);
-		printLinkList(llist);
+		//printLinkList(llist);
 		llist = addbefore(llist, 40, 8);
+		//printLinkList(llist);
+		llist = deletenode(llist, 40 );
+		//printLinkList(llist);
+		llist = deletenodewithkey(llist, 3);
+		//printLinkList(llist);
+		//System.out.println(getListCount(llist));
+		//System.out.println(getListCountRecursive(llist.head));
+		//partial wrong for passing list as argument
+		//llist = swapNodeWithoutData(llist, 10,30);
+		printLinkList(llist);
+		llist = reverseList(llist);
 		printLinkList(llist);
 
+	}
+
+	private static LinkList reverseList(LinkList list) {
+		  Node prev = null;
+	        Node current = list.head;
+	        Node next = null;
+	        while (current != null) {
+	            next = current.next;
+	            current.next = prev;
+	            prev = current;
+	            current = next;
+	        }
+	        
+	        list.head = prev;
+	        return list;
+	}
+
+	private static LinkList swapNodeWithoutData(LinkList list, int i, int j) {
+		if(i==j)
+		{
+			return list;
+		}
+		Node temp = list.head;
+		Node xPrev,xCurr,xNext;
+		
+		while(temp.next.data != i)
+		{
+			temp = temp.next;
+		}
+		
+		xPrev = temp;
+		xCurr = temp.next;
+		xNext = xCurr.next;
+		Node yPrev,yCurr,yNext;
+		temp = list.head;
+		while(temp.next.data != j)
+		{
+			temp = temp.next;
+		}
+		
+		
+		yPrev = temp;
+		yCurr= temp.next;
+		yNext = yCurr.next;
+
+		if(xCurr == null || yCurr == null)
+		{
+			return list;
+		}
+		
+		if (xPrev != null)
+            xPrev.next = yCurr;
+        else //make y the new head
+            list.head = yCurr;
+ 
+        // If y is not head of linked list
+        if (yPrev != null)
+            yPrev.next = xCurr;
+        else // make x the new head
+            list.head = xCurr;
+ 
+        // Swap next pointers
+        temp = xCurr.next;
+        xCurr.next = yCurr.next;
+        yCurr.next = temp;
+		
+		
+		return list;
+	}
+
+	private static int getListCount(LinkList list) {
+		int count  ;
+		Node temp = list.head;
+		for (count = 0; temp!=null; count++) {
+			temp = temp.next;
+		}
+		return count;
+	}
+	private static int getListCountRecursive(Node head) {
+		if(head==null)
+		{
+			return 0;
+		}
+		return getListCountRecursive(head.next)+1; 
+	}
+
+	private static LinkList deletenode(LinkList list, int nodeValue) {
+		Node temp = list.head;
+		Node deletedNode ;
+		if(temp.data == nodeValue)
+		{
+			list.head = list.head.next;
+		}
+		else{
+		while(temp.next.data!= nodeValue)
+		{
+			temp = temp.next;
+		}
+		deletedNode = temp.next;
+		temp.next = deletedNode.next;
+		deletedNode = null;
+		}
+		return list;
+	}
+	private static LinkList deletenodewithkey(LinkList list, int key) {
+		Node temp = list.head;
+		if(key==0)
+		{ 
+			list.head = temp.next;   // Change head
+			return list;
+		}
+		 for (int i=0; temp!=null && i<key-1; i++)
+		 {
+			 temp = temp.next;
+		 }
+		 Node next = temp.next.next;
+		 temp.next = next; 
+		return list;
 	}
 
 	private static void printLinkList(LinkList list) {
--- src/misc/Square.java
+++ src/misc/Square.java
@@ -2,7 +2,7 @@ package misc;
 
 public class Square {
 	public static void main(String[] args) {
-		System.out.println(distributeCadbury(-1,6, 3, 3));
+		System.out.println(distributeCadbury(5,6, 3, 6));
 	}
 
 	public static int distributeCadbury(int input1, int input2, int input3,
@@ -12,32 +12,32 @@ public class Square {
 			int len = input1+i;
 			for (int j = 0; j < input4-input3+1; j++) {
 				int bre = input3+j;
-				totalStudents+=calculateStudents(len, bre, 0);
+				totalStudents+=calculateStudents(len, bre);
 				
 			}
 		}
 		return totalStudents;
 	}
 	
-	public static int calculateStudents(int len, int bre, int count)
+	public static int calculateStudents(int len, int bre)
 	{	
 		if(len ==0 || bre == 0 )
 		{
-			return count;
+			return 0;
 		}
 		if(len==1 || bre==1)
 		{
-			return ((bre>1)? bre : len)+count;
+			return ((bre>1)? bre : len);
 		}
 		else if(len<bre)
 		{
-			count += bre/len;
-			return calculateStudents(bre%len,len,count);
+			
+			return (bre/len)+calculateStudents(bre%len,len);
 		}
 		else
 		{
-			count += len/bre;
-			return calculateStudents(len%bre,bre,count);
+			
+			return (len/bre)+calculateStudents(len%bre,bre);
 		}
 	}
 }
